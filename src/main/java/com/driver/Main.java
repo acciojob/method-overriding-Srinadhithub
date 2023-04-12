c679d8d3865b81873aefad239d225dc077e4042d
Srinadhithub <117295296+Srinadhithub@users.noreply.github.com>
2023-04-12 11:51:42 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,16 @@
 package com.driver;
-
-public class Main {
-  static class A{
+ class A{
     String meth(){
       return "Invoking method from class A";
     }
   }
-  static class B extends A{
+class B extends A{
     String meth(){
       return "Invoking method from class B";
     }
   }
+public class Main {
+  
   public static void main(String args[]){
   B b= new B();
   System.out.println(b.meth());
