1e6b6d0da47e94b621746349c3f055027e654e40
SRINADH <srinadhpodhili@gmail.com>
2023-04-13 11:56:33 +0530

    solve

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class B {
+   public String meth(){
+        return "Invoking method from class A";
+    }
+
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,4 @@
 package com.driver;
- class A{
-    String meth(){
-      return "Invoking method from class A";
-    }
-  }
-class B extends A{
-    String meth(){
-      return "Invoking method from class B";
-    }
-  }
 public class Main {
   
   public static void main(String args[]){
