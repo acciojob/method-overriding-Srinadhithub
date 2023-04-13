48e81d17ac76af6f380b3d9ff7867c769b70f025
Srinadhithub <117295296+Srinadhithub@users.noreply.github.com>
2023-04-13 11:58:29 +0530

    Update B.java

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,8 +1,8 @@
 package com.driver;
 
-public class B {
+public class B extends A{
    public String meth(){
-        return "Invoking method from class A";
+        return "Invoking method from class B";
     }
 
 }
